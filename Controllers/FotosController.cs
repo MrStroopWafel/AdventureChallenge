09024ef09f9b19b6b36993c4e3bd4ebc75527bb3
MrStroopWafel <larsmulleneers@hotmail.nl>
2022-09-17 14:28:09 +0200

    Alle resterende basis db controllers toegevoegd met de daarbijzijnde crud functionalitijd

---
+++ Controllers/FotosController.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using AdventureChallenge.Models;
+
+namespace AdventureChallenge.Controllers
+{
+    public class FotosController : Controller
+    {
+        private readonly AdventureChallengeContext _context;
+
+        public FotosController(AdventureChallengeContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Fotos
+        public async Task<IActionResult> Index()
+        {
+              return View(await _context.Fotos.ToListAsync());
+        }
+
+        // GET: Fotos/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Fotos == null)
+            {
+                return NotFound();
+            }
+
+            var foto = await _context.Fotos
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (foto == null)
+            {
+                return NotFound();
+            }
+
+            return View(foto);
+        }
+
+        // GET: Fotos/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Fotos/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Foto1")] Foto foto)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(foto);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(foto);
+        }
+
+        // GET: Fotos/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Fotos == null)
+            {
+                return NotFound();
+            }
+
+            var foto = await _context.Fotos.FindAsync(id);
+            if (foto == null)
+            {
+                return NotFound();
+            }
+            return View(foto);
+        }
+
+        // POST: Fotos/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Foto1")] Foto foto)
+        {
+            if (id != foto.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(foto);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!FotoExists(foto.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(foto);
+        }
+
+        // GET: Fotos/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Fotos == null)
+            {
+                return NotFound();
+            }
+
+            var foto = await _context.Fotos
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (foto == null)
+            {
+                return NotFound();
+            }
+
+            return View(foto);
+        }
+
+        // POST: Fotos/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Fotos == null)
+            {
+                return Problem("Entity set 'AdventureChallengeContext.Fotos'  is null.");
+            }
+            var foto = await _context.Fotos.FindAsync(id);
+            if (foto != null)
+            {
+                _context.Fotos.Remove(foto);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool FotoExists(int id)
+        {
+          return _context.Fotos.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ Controllers/UserChallengesController.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using AdventureChallenge.Models;
+
+namespace AdventureChallenge.Controllers
+{
+    public class UserChallengesController : Controller
+    {
+        private readonly AdventureChallengeContext _context;
+
+        public UserChallengesController(AdventureChallengeContext context)
+        {
+            _context = context;
+        }
+
+        // GET: UserChallenges
+        public async Task<IActionResult> Index()
+        {
+            var adventureChallengeContext = _context.UserChallenges.Include(u => u.Challenge).Include(u => u.Foto).Include(u => u.User);
+            return View(await adventureChallengeContext.ToListAsync());
+        }
+
+        // GET: UserChallenges/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.UserChallenges == null)
+            {
+                return NotFound();
+            }
+
+            var userChallenge = await _context.UserChallenges
+                .Include(u => u.Challenge)
+                .Include(u => u.Foto)
+                .Include(u => u.User)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (userChallenge == null)
+            {
+                return NotFound();
+            }
+
+            return View(userChallenge);
+        }
+
+        // GET: UserChallenges/Create
+        public IActionResult Create()
+        {
+            ViewData["ChallengeId"] = new SelectList(_context.Challenges, "Id", "Id");
+            ViewData["FotoId"] = new SelectList(_context.Fotos, "Id", "Id");
+            ViewData["UserId"] = new SelectList(_context.Users, "Id", "Id");
+            return View();
+        }
+
+        // POST: UserChallenges/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,UserId,ChallengeId,Beschrijving,FotoId,Afgerond")] UserChallenge userChallenge)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(userChallenge);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ChallengeId"] = new SelectList(_context.Challenges, "Id", "Id", userChallenge.ChallengeId);
+            ViewData["FotoId"] = new SelectList(_context.Fotos, "Id", "Id", userChallenge.FotoId);
+            ViewData["UserId"] = new SelectList(_context.Users, "Id", "Id", userChallenge.UserId);
+            return View(userChallenge);
+        }
+
+        // GET: UserChallenges/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.UserChallenges == null)
+            {
+                return NotFound();
+            }
+
+            var userChallenge = await _context.UserChallenges.FindAsync(id);
+            if (userChallenge == null)
+            {
+                return NotFound();
+            }
+            ViewData["ChallengeId"] = new SelectList(_context.Challenges, "Id", "Id", userChallenge.ChallengeId);
+            ViewData["FotoId"] = new SelectList(_context.Fotos, "Id", "Id", userChallenge.FotoId);
+            ViewData["UserId"] = new SelectList(_context.Users, "Id", "Id", userChallenge.UserId);
+            return View(userChallenge);
+        }
+
+        // POST: UserChallenges/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,UserId,ChallengeId,Beschrijving,FotoId,Afgerond")] UserChallenge userChallenge)
+        {
+            if (id != userChallenge.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(userChallenge);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!UserChallengeExists(userChallenge.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ChallengeId"] = new SelectList(_context.Challenges, "Id", "Id", userChallenge.ChallengeId);
+            ViewData["FotoId"] = new SelectList(_context.Fotos, "Id", "Id", userChallenge.FotoId);
+            ViewData["UserId"] = new SelectList(_context.Users, "Id", "Id", userChallenge.UserId);
+            return View(userChallenge);
+        }
+
+        // GET: UserChallenges/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.UserChallenges == null)
+            {
+                return NotFound();
+            }
+
+            var userChallenge = await _context.UserChallenges
+                .Include(u => u.Challenge)
+                .Include(u => u.Foto)
+                .Include(u => u.User)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (userChallenge == null)
+            {
+                return NotFound();
+            }
+
+            return View(userChallenge);
+        }
+
+        // POST: UserChallenges/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.UserChallenges == null)
+            {
+                return Problem("Entity set 'AdventureChallengeContext.UserChallenges'  is null.");
+            }
+            var userChallenge = await _context.UserChallenges.FindAsync(id);
+            if (userChallenge != null)
+            {
+                _context.UserChallenges.Remove(userChallenge);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool UserChallengeExists(int id)
+        {
+          return _context.UserChallenges.Any(e => e.Id == id);
+        }
+    }
+}
